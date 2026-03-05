954c19f5dec450e277517f08de58b8e5bf194f07
Clies93 <cliver.dares@hotmail.com>
2026-03-05 10:19:13 +0100

    script factorielle

---
+++ scripts/factorielle.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+read -p "Entrez un entier positif : " n
+
+# Vérification
+if ! [[ "$n" =~ ^[0-9]+$ ]]; then
+    echo "Erreur : '$n' n'est pas un entier positif."
+    exit 1
+fi
+
+fact=1
+for (( i=2; i<=n; i++ )); do
+    fact=$((fact * i))
+done
+
+echo "Factorielle de $n = $fact"
